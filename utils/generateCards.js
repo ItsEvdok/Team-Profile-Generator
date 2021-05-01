225552a99c36b65378919472d1008a16632e3888
ItsEvdok <semionaevdokimov@icloud.com>
2021-05-01 12:07:16 -0400

    Update generateCards.js

--- utils/generateCards.js
+++ utils/generateCards.js
@@ -1,6 +1,7 @@
 const Manager = require('../lib/Manager');
 const Engineer = require('../lib/Engineer');
 const Intern = require('../lib/Intern');
+const fs = require('fs');
 
 function getManagerData(showManagerData) {
     const{name, id, email, officeNumber} = showManagerData;
